8e0b0bd72a738cb96635e91e1eb3c2b9fdc61bb6
VolodymyrPerun <47578369+VolodymyrPerun@users.noreply.github.com>
2019-07-16 19:26:21 +0300

    innit

--- upqode-task-master/js/main.js
+++ upqode-task-master/js/main.js
@@ -188,3 +188,21 @@ $('body').scrollspy({ target: '#navbar-example' })
 
 
 
+
+formData = {
+	'name'     : $('input[name=name]').val(),
+	'email'    : $('input[name=email]').val(),
+	'subject'  : $('input[name=subject]').val(),
+	'message'  : $('textarea[name=message]').val(),
+	'cmethod'  : $('input:radio[name=cmethod]:checked').val()
+  };
+
+
+
+
+
+
+
+
+
+
