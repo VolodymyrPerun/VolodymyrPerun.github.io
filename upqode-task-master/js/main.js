17f04558d5ff361c0113b51c9be021f690861ba5
VolodymyrPerun <47578369+VolodymyrPerun@users.noreply.github.com>
2019-07-16 04:05:36 +0300

    innit

--- upqode-task-master/js/main.js
+++ upqode-task-master/js/main.js
@@ -1,4 +1,43 @@
+
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+	location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+	&& 
+	location.hostname == this.hostname
+  ) {
+	// Figure out element to scroll to
+	var target = $(this.hash);
+	target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	// Does a scroll target exist?
+	if (target.length) {
+	  // Only prevent default if animation is actually gonna happen
+	  event.preventDefault();
+	  $('html, body').animate({
+		scrollTop: target.offset().top
+	  }, 2000, function() {
+		// Callback after animation
+		// Must change focus!
+		var $target = $(target);
+		$target.focus();
+		if ($target.is(":focus")) { // Checking if the target was focused
+		  return false;
+		} else {
+		  $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+		  $target.focus(); // Set focus again
+		};
+	  });
+	}
+  }
+});
+
+
 // navMenu
+
 (function ($) {
 	$('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
 		if (!$(this).next().hasClass('show')) {
@@ -50,11 +89,21 @@ function setDelay() {
 
 
 
+$(document).ready(function() {
+	$('.level').waypoint(function() {
+	$('.level').css({
+	animation: "animate-positive 7s",
+	opacity: "1"
+	});
+	}, { offset: '75%' });
+
+});
+
 
 document.addEventListener("DOMContentLoaded", function () {
 
-	var progressBar = document.querySelectorAll(".progress-bar");
-	var time = 5000;
+	var progressBar = document.querySelectorAll(".skill-bar");
+	var time = 7000;
 
 
 	progressBar.forEach(function (i) {
@@ -77,6 +126,12 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+
+
+
+
+
+
 // owlCarousel iPads
 
 		 
@@ -95,3 +150,41 @@ $(function () {
 		smartSpeed: 1000, // duration of change of 1 slide
 	});
 });
+
+
+
+
+
+$(function() {
+	// при нажатии на кнопку scrollup
+	$('.scrollup').click(function() {
+	  // переместиться в верхнюю часть страницы
+	  $("html, body").animate({
+		scrollTop:0
+	  },2000);
+	})
+  })
+  // при прокрутке окна (window)
+  $(window).scroll(function() {
+	// если пользователь прокрутил страницу более чем на 200px
+	if ($(this).scrollTop()>200) {
+	  // то сделать кнопку scrollup видимой
+	  $('.scrollup').fadeIn();
+	}
+	// иначе скрыть кнопку scrollup
+	else {
+	  $('.scrollup').fadeOut();
+	}
+  });
+
+  $('html, body').animate({
+    scrollTop: $("#target-element").offset().top
+}, 2000);
+
+
+
+
+$('body').scrollspy({ target: '#navbar-example' })
+
+
+
