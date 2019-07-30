e9d45d8430e2336c708d69df7a702b813b6cb451
VolodymyrPerun <47578369+VolodymyrPerun@users.noreply.github.com>
2019-07-30 21:04:29 +0300

    innit

--- bicycle-master/js/common.js
+++ bicycle-master/js/common.js
@@ -1,5 +1,12 @@
-$(function() {
-
-	// Custom JS
-
+/*==========Owl Carousel Banners =========*/
+var owl = $(".main-demo .owl-carousel").owlCarousel({
+	autoplayTimeout: 6000,
+	autoplay: true,
+	items: 1,
+	loop: true,
+	dots: true,
+	nav: true,
+	onDragged: setDelay,
+	rtl: true,
+	navText: ["", ""],
 });
