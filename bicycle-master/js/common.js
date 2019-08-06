f3c2b1bdcedbb11d8d32dacb8b53de61f9a7ff1f
VolodymyrPerun <47578369+VolodymyrPerun@users.noreply.github.com>
2019-08-06 16:09:20 +0300

    innit

--- bicycle-master/js/common.js
+++ bicycle-master/js/common.js
@@ -1,15 +1,15 @@
 $(function () {
 	// Owl Carousel
-	var owl = $(".ipads-demo .owl-carousel");
+	var owl = $(".main-demo .owl-carousel");
 	owl.owlCarousel({
 		items: 1,
 		nav: false,
 		autoplay: true,
 		loop: true,
 		margin: 0,
-		dots: false,
+		dots: true,
 		rtl: true,
 		autoplay: 100000, // time for slides changes
-		smartSpeed: 1000, // duration of change of 1 slide
+		smartSpeed: 5000, // duration of change of 1 slide
 	});
 });
